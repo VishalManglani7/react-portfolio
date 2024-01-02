f2f7e8e0d06d3bf7cef1b53e243614b355afb92e
Vishal <vishal.manglani7@gmail.com>
2024-01-02 18:59:41 -0500

    updated page so that it now displays with react. place holders on page now

---
+++ react-portfolio/src/components/Portfolio.jsx
@@ -0,0 +1,34 @@
+import Project from "./Project";
+
+function Portfolio() {
+  // fill in the required data, image file should be in the assets/projects folder and name should match the "default as " statment in index.js
+  const projects = [
+    {
+      name: "project 1 test",
+      description: "HTML/JS/CSS",
+      link: "https://heroku.com",
+      repo: "https://github.com/femke77",
+      image: "cat",
+    },
+    {
+      name: "project 2 test",
+      description: "NRG Stack",
+      link: "https://heroku.com/",
+      repo: "https://github.com/femke77",
+      image: "cat",
+    },
+  ];
+
+  // for each project, use the Project component to build a project
+  return (
+    <div>
+      <div className="d-flex">
+        {projects.map((project) => (
+          <Project project={project} key={"project-" + project.name} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
