3b61f77e62fe752672b8280939ca5bd45dc06722
Vishal <vishal.manglani7@gmail.com>
2024-01-14 14:41:19 -0500

    deleted unused assets. updated notes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
 function Resume() {
+  const proficiencies = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+  ];
+
+
   return (
     <div className="ResumeClass" id="Resume">
       <h2 className="title">
         Resume
       </h2>
       <div className="resume-content">
-        <p>Download my Resume - Resume to be added soon: <a href="../assets/projects/resume" download>Resume.pdf</a></p>
+        <p>Download my Resume - Resume to be added soon: <a href="../assets/projects/resume.html" download>Resume.html</a></p>
+        <h3>Coding Proficiencies:</h3>
+        <ul className="proficiency-list">
+          {proficiencies.map((proficiency, index) => (
+            <li key={index}>{proficiency}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
